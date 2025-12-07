2a1cb0dcb169e757141133fed9cbdaa6529d8427
Frank Teheran <frankteheran.contacto@gmail.com>
2025-12-07 08:33:28 -0500

    first commit

---
+++ corteum/src/App.jsx
@@ -0,0 +1,30 @@
+
+function App() {
+
+
+  return (
+    
+
+
+    <div className="hero">
+      <div className="hero-content">
+          <h1>Corteum</h1>
+          <p className="result-content" >0.0</p>
+          <div className="data-cut">
+            <input type="text" className="cut-content"/>
+            <input type="text" className="cut-content"/>
+            <input type="text" className="cut-content"/>
+            <input type="text" className="cut-content"/>
+          </div>
+      </div>
+      <div className="info-content">
+        <p className="p-info">
+          Cada cuadrícula está configurada con el porcentaje correspondiente a cada corte, lo que hace más fácil realizar los cálculos.
+        </p>
+      </div>
+        
+    </div>
+  )
+}
+
+export default App
