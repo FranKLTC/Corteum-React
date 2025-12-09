e4efb7d9f5936979119bc37a0cd57b0a8576e251
Frank Teheran <frankteheran.contacto@gmail.com>
2025-12-08 22:39:19 -0500

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://frankltc.github.io/Corteum-React/',
+  base: '/Corteum-React/',
 })
